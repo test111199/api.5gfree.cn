c5b792d20b07f4650874d9b2c24cf849839ade4f
test111199 <test111199@outlook.com>
2018-02-26 11:56:20 +0800

    2018 Feb 26 11:56

--- libs/regUsimFunction.php
+++ libs/regUsimFunction.php
@@ -8,7 +8,7 @@ function checkIccidExists(&$getArray,$arrayRow)
 
 echo "第".$arrayRow."次:";
 //var_dump($getArray);
-echo "显示获得的二维数组内容为：".$getArray[$arrayRow]["A"]."ICCID: ".$getArray[$arrayRow]["B"]."<br>\n";
+echo "显示获得的二维数组内容为：".$getArray[$arrayRow]["A"]."数据使用量: ".$getArray[$arrayRow]["D"];
 $timesCount = $arrayRow + 1;
 return $timesCount;
 
