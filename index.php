f3354b5e9141f3486e89b7fe8b291b79ba1f7465
test111199 <test111199@outlook.com>
2018-03-30 17:27:38 +0800

    2018 Mar 30 17:27

--- index.php
+++ index.php
@@ -83,8 +83,8 @@
           <div class="well sidebar-nav">
             <ul class="nav nav-list">
 <?php  
-    $sqlStr = "SELECT IoT_Menu.menuStr FROM IoT_Menu WHERE menuLevelID IN (SELECT IoT_Role.roleItems from IoT_Role WHERE roleID = ".$_COOKIE['loginRole'].")";
-//    $sqlStr = "SELECT IoT_Menu.menuStr FROM IoT_Menu WHERE menuLevelID IN (10,11,12)";
+//    $sqlStr = "SELECT IoT_Menu.menuStr FROM IoT_Menu WHERE menuLevelID IN (SELECT IoT_Role.roleItems from IoT_Role WHERE roleID = ".$_COOKIE['loginRole'].")";
+    $sqlStr = "SELECT IoT_Menu.menuStr FROM IoT_Menu WHERE menuLevelID IN (10,11,12)";
 echo $sqlStr;
     $res=mysqli_query($myconn,$sqlStr); 
 echo $res;
